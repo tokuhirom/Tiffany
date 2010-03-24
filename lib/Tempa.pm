47a360c8038157382ae428cf3a74ef61dba70806
tokuhirom <tokuhirom@gmail.com>
2010-03-24 22:16:16 +0900

    initial import

---
+++ lib/Tempa.pm
@@ -0,0 +1,39 @@
+package Tempa;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Tempa -
+
+=head1 SYNOPSIS
+
+  use Tempa;
+
+=head1 DESCRIPTION
+
+Tempa is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
